12d553fbaf2d0102b497429003f28b6a5deeb9fd
jonas07br <jonasrafa07@gmail.com>
2024-11-26 20:56:30 -0300

    change login-component path

--- DesafioNutAngular/src/app/app.routes.ts
+++ DesafioNutAngular/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { MetricsViewComponent } from './components/metrics-view/metrics-view.component';
 
 export const routes: Routes = [
