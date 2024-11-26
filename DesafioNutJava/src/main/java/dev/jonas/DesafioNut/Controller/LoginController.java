478f973c15cadcfcac6a28b5f00b9d1da1e371f2
jonas07br <jonasrafa07@gmail.com>
2024-11-26 01:20:34 -0300

    update login method

--- DesafioNutJava/src/main/java/dev/jonas/DesafioNut/Controller/LoginController.java
+++ DesafioNutJava/src/main/java/dev/jonas/DesafioNut/Controller/LoginController.java
@@ -1,6 +1,10 @@
 package dev.jonas.DesafioNut.Controller;
 
+import java.security.Security;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,16 +21,29 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("/login")
-    public Section login(@RequestBody User user){
-        return loginService.createToken(user);
+    // @PostMapping("/login")
+    // public Section login(@RequestBody User user){
+    //     return loginService.createToken(user);
+    // }
+
+    @GetMapping("/login")
+    public Section login(){
+        String[] roles = SecurityContextHolder
+            .getContext()
+            .getAuthentication()
+            .getAuthorities()
+            .stream().map(authority -> 
+                authority.getAuthority().replace("ROLE_", "")).toArray(String[]::new);
+        
+        String login = SecurityContextHolder
+            .getContext()
+            .getAuthentication()
+            .getName();
+        return loginService.generateToken(login, roles);
     }
 
     @GetMapping("/teste")
-    public User teste(){
-        User teste = new User();
-        teste.setLogin("TESTE");
-        teste.setPassword("TESTE");
-        return teste;
+    public String teste(){
+        return "AREA MANAGER";
     }
 }
